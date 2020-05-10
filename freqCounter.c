6996592bc940a481d84d8c2e00523af7d3b131d5
Diogo Alpendre <37402115+dalpendre@users.noreply.github.com>
2020-05-10 11:50:47 +0100

    Version 1.6
    freqCounter.c
    
    Adicionada função de matriz para guardar contagem dos bytes no modo 1

--- freqCounter.c
+++ freqCounter.c
@@ -3,12 +3,15 @@
 #include <string.h>
 
 #include "debug.h"
+#include "memory.h"
 
 #include "freqCounter.h"
 
 //Text hightlights
 #define RED   "\x1B[31m"
 #define RESET "\x1B[0m"
+
+#define MODE1_NUM_ROWS 256
 
 int verify_parameters(int argc, char *argv[])
 {
@@ -58,29 +61,55 @@ int verify_mode_and_process_file(FILE *fptr, int mode_number, char *file_path)
     else if(mode_number == 4)
         process_listed_files_mode4(fptr, file_path);
     else
-        ERROR(1, RED "ERROR: Invalid mode number\n" RESET);
+        ERROR(1, "ERROR: invalid value ‘%d’ for -m/--mode.\n", mode_number);
 
     return 0;
-}
+} 
 
 int process_listed_files_mode1(FILE *fptr, char *file_path)
 {
-    char ch;
+    char c, file_caracther;
 
-    while((ch = fgetc(fptr)) != EOF)
-        printf("%c", ch);
-
-    verify_bytes(fptr);
+    printf("%s\n", file_path);
+    while((file_caracther = fgetc(fptr)) != EOF)
+    {
+        for(c = 'A'; c <= 'Z'; c++)
+        {
+            if(c == file_caracther)
+            {
+                
+            }
+        }
+    }
 
     fclose(fptr);
 
     return 0;
 }
 
-int verify_byte_mode1(FILE *fptr)
+//Matrix to store ocurrences of each byte
+int **matrix_mode1_new(void)
 {
-    return 0;
+    size_t mem_len = MODE1_NUM_ROWS * 2 * sizeof(int);
+    int *matrix_ptr = malloc(mem_len);
+
+    size_t vect_len = sizeof(int*) * MODE1_NUM_ROWS;
+    int **matrix_vect_ptr = MALLOC(vect_len);
+
+    if(matrix_ptr == NULL || matrix_vect_ptr == NULL)
+    {
+        exit(1);
+    }
+
+    for(int row = 0; row < MODE1_NUM_ROWS; row++)
+    {
+        matrix_vect_ptr[row] = &(matrix_ptr[row * MODE1_NUM_ROWS]);
+    }
+
+    return matrix_vect_ptr;
 }
+
+
 
 int process_listed_files_mode2(FILE *fptr, char *file_path)
 {
--- freqCounter.h
+++ freqCounter.h
@@ -5,7 +5,6 @@ int verify_parameters(int argc, char *argv[]);
 int verify_if_file_exists(int mode_number, char *file_path);
 int verify_mode_and_process_file(FILE *fptr, int mode_number, char *filename);
 int process_listed_files_mode1(FILE *fptr, char *file_path);
-int verify_bytes_mode1(FILE *fptr);
 int process_listed_files_mode2(FILE *fptr, char *file_path);
 int process_listed_files_mode4(FILE *fptr, char *file_path);
 
